49750df71706ad100badc85eaa2380b670a39ca7
SergioBurlaka <burlaka01@gmail.com>
2020-03-23 15:18:41 +0200

    Updates

---
+++ precache-manifest.7eaf329258bafb2af4db4b1ea704fa80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9b51ef3bce227db23c4941c86687d0",
+    "url": "/ladys-shop/index.html"
+  },
+  {
+    "revision": "13f8f1a6fdea4c822c12",
+    "url": "/ladys-shop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "457ccfea01fbd2e32c6b",
+    "url": "/ladys-shop/static/js/2.d35f0429.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ladys-shop/static/js/2.d35f0429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f8f1a6fdea4c822c12",
+    "url": "/ladys-shop/static/js/main.5c641642.chunk.js"
+  },
+  {
+    "revision": "098a4697d6a8ae749ea9",
+    "url": "/ladys-shop/static/js/runtime-main.378e0c89.js"
+  }
+]);
