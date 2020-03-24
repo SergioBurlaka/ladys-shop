949cf29de11748e236637b5480ad8990887731fc
SergioBurlaka <burlaka01@gmail.com>
2020-03-24 23:43:48 +0200

    Updates

---
+++ precache-manifest.435cb5f2672cce6e7afc782b227093f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aed882c760254346410d77eb84e2af4",
+    "url": "/ladys-shop/index.html"
+  },
+  {
+    "revision": "e85c943bf1cabe0dcb72",
+    "url": "/ladys-shop/static/css/main.59b2bb9a.chunk.css"
+  },
+  {
+    "revision": "65b518cfb489fa7c296d",
+    "url": "/ladys-shop/static/js/2.da760fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ladys-shop/static/js/2.da760fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85c943bf1cabe0dcb72",
+    "url": "/ladys-shop/static/js/main.412a9878.chunk.js"
+  },
+  {
+    "revision": "098a4697d6a8ae749ea9",
+    "url": "/ladys-shop/static/js/runtime-main.378e0c89.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ladys-shop/precache-manifest.88cf1a5d6b6cbeba460fc1c5e4c5ab95.js"
+  "/ladys-shop/precache-manifest.435cb5f2672cce6e7afc782b227093f3.js"
 );
 
 self.addEventListener('message', (event) => {
